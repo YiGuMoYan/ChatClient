621edc6e36f9c1aa462f5017ae95e39573d07703
YiGuMoYan <3194775246@qq.com>
2023-01-13 17:18:29 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-<div></div>
+  <div>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+</script>
 
 <style>
 </style>
---
+++ src/components/WebSocket.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'WebSocket',
+  setup () {
+  },
+  data () {
+    return {
+      webSocketUrl: 'ws://127.0.0.1:8080/login/'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
