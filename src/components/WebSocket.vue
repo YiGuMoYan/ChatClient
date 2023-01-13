9320e6699c716ea93fdfe8b77dd1ae93ce55e61a
YiGuMoYan <3194775246@qq.com>
2023-01-13 23:28:56 +0800

    websocket ok

--- src/components/WebSocket.vue
+++ src/components/WebSocket.vue
@@ -1,15 +1,33 @@
 <template>
-
+  <div>WebSocket</div>
 </template>
 
 <script>
 export default {
-  name: 'WebSocket',
-  setup () {
+  mounted () {
+    this.init()
   },
   data () {
     return {
-      webSocketUrl: 'ws://127.0.0.1:8080/login/'
+      webSocketUrl: 'ws://127.0.0.1:8080/login/1613829011638091778',
+      socket: ''
+    }
+  },
+  methods: {
+    init () {
+      if (typeof (WebSocket) === 'undefined') {
+        alert('您的浏览器不支持')
+      } else {
+        this.socket = new WebSocket(this.webSocketUrl)
+        this.socket.onopen = this.open
+        this.socket.onerror = this.error
+      }
+    },
+    open () {
+      console.log('连接成功')
+    },
+    error () {
+      console.log('连接错误')
     }
   }
 }
