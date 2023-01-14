9c4acc68d03c4f27e263f384ac52e87d34b756af
YiGuMoYan <3194775246@qq.com>
2023-01-14 13:14:15 +0800

    add query

--- src/main.js
+++ src/main.js
@@ -9,11 +9,14 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import '@/assets/css/global.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
 
+import $ from 'jquery'
+
 const app = createApp(App)
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
+
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
